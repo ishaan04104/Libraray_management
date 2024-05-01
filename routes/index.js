a678d994197859acf080e19e6192d806cdae8018
ishaan04104 <89799133+ishaan04104@users.noreply.github.com>
2024-05-02 03:40:24 +0530

    Initial summit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Book = require('../models/book')
 
+// Get all books
 router.get('/', async (req, res) => {
   let books
   try {
@@ -12,28 +13,59 @@ router.get('/', async (req, res) => {
   res.render('index', { books: books })
 })
 
+// Render create page
+router.get('/create', (req, res) => {
+  res.render('create');
+});
+
+// Render login page
 router.get('/login', (req, res) => {
   res.render('login');
 });
+
+// Render 404 page
+router.get('/404', (req, res) => {
+  res.render('404');
+});
+
+// Render account 404 page
+router.get('/404acc', (req, res) => {
+  res.render('404acc');
+});
+
+// Users object to store username-password pairs
+const users = {}
+
+// Handle login POST request
 router.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  if(username =="21BIT" && password=="password"){
+  if(username in users && users[username] == password) {
+    console.log("Login successful");
     req.session.user = {
       id: username,
       username: username,
       password: password
     }
     res.redirect("/")
-  }else{
+  } else {
+    res.redirect("/404")
+  }
+})
+
+// Handle create POST request
+router.post('/create', (req, res) => {
+  const { username, password } = req.body;
+
+  if(username in users) {
+    res.redirect("/404acc")
+  } else {
+    users[username] = password
+    console.log(users)
     res.redirect("/login")
   }
+})
 
-  // Check if the username and password are valid
-  // If they are, redirect the user to the home page
-  // If they're not, render the login page again with an error message
-});
 
 
 module.exports = router
-
